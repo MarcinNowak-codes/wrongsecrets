447865c2f75134ef6355b2adab98952b46f626cf
Marcin Nowak <marcin.nowak@techmetria.pl>
2022-12-09 20:23:22 +0100

    Spring Boot 2.7.x reduction

--- 2.7.x/src/main/java/org/owasp/wrongsecrets/HerokuWebSecurityConfig.java
+++ 2.7.x/src/main/java/org/owasp/wrongsecrets/HerokuWebSecurityConfig.java
@@ -1,23 +1,18 @@
 package org.owasp.wrongsecrets;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
-public class HerokuWebSecurityConfig {
+@Order(1)
+public class HerokuWebSecurityConfig extends WebSecurityConfigurerAdapter {
 
-    @Bean
-    @Order(1)
-    public SecurityFilterChain configureHerokuWebSecurity(HttpSecurity http) throws Exception {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
         http.requiresChannel()
-            .requestMatchers(r ->
-                r.getRequestURL().toString().contains("heroku")
-                    && (r.getHeader("x-forwarded-proto") != null || r.getHeader("X-Forwarded-Proto") != null)
-            )
+            .requestMatchers(r -> r.getRequestURL().toString().contains("heroku") && (r.getHeader("x-forwarded-proto") != null || r.getHeader("X-Forwarded-Proto") != null))
             .requiresSecure();
-        return http.build();
     }
 }
