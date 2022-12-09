8ddbd023a67821e3d598fb223e27976d28c00c6c
Marcin Nowak <marcin.nowak@techmetria.pl>
2022-12-09 20:33:06 +0100

    Spring Boot 2.7.x reduction

--- 2.7.x/src/test/java/org/owasp/wrongsecrets/HerokuWebSecurityConfigTest.java
+++ 2.7.x/src/test/java/org/owasp/wrongsecrets/HerokuWebSecurityConfigTest.java
@@ -5,10 +5,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.boot.web.server.LocalServerPort;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.ResourceAccessException;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.fail;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -32,14 +30,5 @@ public class HerokuWebSecurityConfigTest {
         } catch (ResourceAccessException e) {
             assert (e.getCause().getCause().toString()).contains("Redirect");
         }
-    }
-
-    @Test
-    void shouldNotRedirectwhenProtoNotProvided() {
-        var restTemplate = builder
-            .build();
-        var rootAddress = "http://localhost:" + port + "/";
-        ResponseEntity entity = restTemplate.getForEntity(rootAddress, String.class);
-        assertTrue(entity.getStatusCode().is2xxSuccessful());
     }
 }
