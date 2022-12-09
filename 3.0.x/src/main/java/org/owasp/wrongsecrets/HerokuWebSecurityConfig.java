dcb6c38e2749b0250b518992ff733c56177f7592
Marcin Nowak <marcin.nowak@techmetria.pl>
2022-12-09 19:00:31 +0100

    Spring Boot 3.0.x and 2.7.x

---
+++ 3.0.x/src/main/java/org/owasp/wrongsecrets/HerokuWebSecurityConfig.java
@@ -0,0 +1,23 @@
+package org.owasp.wrongsecrets;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class HerokuWebSecurityConfig {
+
+    @Bean
+    @Order(1)
+    public SecurityFilterChain configureHerokuWebSecurity(HttpSecurity http) throws Exception {
+        http.requiresChannel()
+            .requestMatchers(r ->
+                r.getRequestURL().toString().contains("heroku")
+                    && (r.getHeader("x-forwarded-proto") != null || r.getHeader("X-Forwarded-Proto") != null)
+            )
+            .requiresSecure();
+        return http.build();
+    }
+}
---
+++ 3.0.x/src/main/java/org/owasp/wrongsecrets/WrongSecretsApplication.java
@@ -0,0 +1,13 @@
+package org.owasp.wrongsecrets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WrongSecretsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WrongSecretsApplication.class, args);
+    }
+
+}
