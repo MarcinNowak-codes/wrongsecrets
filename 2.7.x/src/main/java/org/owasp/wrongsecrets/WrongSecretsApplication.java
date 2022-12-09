81c45c766ff604f688a9eab88aa4eba452b24ff0
Marcin Nowak <marcin.nowak@techmetria.pl>
2022-12-09 21:05:04 +0100

    Spring Boot 2.7.x reduction

--- 2.7.x/src/main/java/org/owasp/wrongsecrets/WrongSecretsApplication.java
+++ 2.7.x/src/main/java/org/owasp/wrongsecrets/WrongSecretsApplication.java
@@ -4,9 +4,6 @@ import org.owasp.wrongsecrets.challenges.kubernetes.Vaultpassword;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Scope;
-import org.springframework.context.annotation.ScopedProxyMode;
 
 @SpringBootApplication
 @EnableConfigurationProperties(Vaultpassword.class)
@@ -15,13 +12,5 @@ public class WrongSecretsApplication {
     public static void main(String[] args) {
         SpringApplication.run(WrongSecretsApplication.class, args);
     }
-
-    @Bean
-    @Scope(value = "session", proxyMode = ScopedProxyMode.TARGET_CLASS)
-    public InMemoryScoreCard scoreCard() {
-        return new InMemoryScoreCard(22);
-    }
-
-
 
 }
